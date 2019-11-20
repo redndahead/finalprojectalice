857bcd19e6fdb4343da6c31a7b0939bc89540a5f
Adam Moore <redndahead@gmail.com>
2019-11-20 09:44:30 -0800

    Initial Commit

---
+++ Finalprojectalice.py
@@ -0,0 +1,37 @@
+import json
+
+import core.base.Managers as managers
+from core.base.model.Intent import Intent
+from core.base.model.Module import Module
+from core.dialog.model.DialogSession import DialogSession
+
+
+class Finalprojectalice(Module):
+	"""
+	Author: Redndahead
+	Description: Takes care of final project
+	"""
+
+	def __init__(self):
+		self._SUPPORTED_INTENTS	= [
+		]
+
+		super().__init__(self._SUPPORTED_INTENTS)
+
+
+	def onMessage(self, intent: str, session: DialogSession):
+
+		sessionId = session.sessionId
+		siteId = session.siteId
+		slots = session.slots
+
+	def onBooted(self):
+		self.randomlySpeak(init=True)
+
+	def randomlySpeak(self, init: bool = False):
+		rnd = 10
+		self.ThreadManager.doLater(interval=rnd, func=self.randomlySpeak)
+		self.logInfo(f'Scheduled next random speaking in {rnd} seconds')
+
+		if not init and not self.UserManager.checkIfAllUser('goingBed') and not self.UserManager.checkIfAllUser('sleeping'):
+			self.say(self.randomTalk(f'randomlySpeakAnger'), siteId='all')
---
+++ README.md
@@ -0,0 +1,37 @@
+# Finalprojectalice
+
+### Download
+
+##### > WGET method
+```bash
+wget http://bit.ly/????????? -O ~/ProjectAlice/system/moduleInstallTickets/Finalproject.install
+```
+
+##### > Alice CLI method
+```bash
+./alice add module Redndahead Finalproject
+```
+
+### Description
+Takes care of final project
+
+- Version: 0.0.1
+- Author: Redndahead
+- Maintainers: N/A
+- Alice minimum version: 1.0.0
+- Conditions:
+  - en
+- Pip requirements: N/A
+- System requirements: N/A
+
+### Configuration
+
+
+`foo`:
+ - type: `bar`
+ - desc: `baz`
+
+`bar`:
+ - type: `baz`
+ - desc: `bar`
+
