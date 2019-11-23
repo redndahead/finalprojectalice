a3f6aabfc855c3bc027b6f3c15caa8123bb27afd
Adam Moore <redndahead@gmail.com>
2019-11-23 12:23:25 -0800

    Try some better debugging.

--- finalprojectalice.py
+++ finalprojectalice.py
@@ -52,5 +52,5 @@ class finalprojectalice(Module):
 		self.logInfo(f'Calendar: {eventsOutput}')
 
 		for event in all_events:
-			self.logInfo(f'{event}')
+			self.logInfo(f'{event["summary"]}')
 			self.say(f'Event name: {event.summary}')
