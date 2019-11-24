6853f761a492314d85613fdf7d84c8103d3a0450
Adam Moore <redndahead@gmail.com>
2019-11-23 23:09:19 -0800

    Add specific time formatting.

--- FinalProjectAlice.py
+++ FinalProjectAlice.py
@@ -77,6 +77,25 @@ class FinalProjectAlice(Module):
 		#	self.logInfo(f'{event["summary"]}')
 		#	self.say(f'Event name: {event["summary"]}. Event Start: {event["start"]}')
 
+	def formatTimeToVoice(self, time=''):
+		date = datetime.strptime(time, "%Y-%m-%dT%H:%M:%S%z")
+
+		time = str(date.hour)
+		if date.hour == 0:
+			time = "12"
+		elif date.hour > 12:
+			time = str(date.hour - 12)
+
+		ampm = "a m"
+		if date.hour >= 12:
+			ampm = "p m"
+
+		if date.minute > 0:
+			time = time + ' ' + str(date.minute)
+
+		time = time + ' ' + ampm
+		return time
+
 	@IntentHandler('NextMeeting')
 	def nextMeeting(self, session: DialogSession, **_kwargs):
 		eventList = json.loads(self.getConfig('eventList'))
@@ -84,23 +103,6 @@ class FinalProjectAlice(Module):
 		self.logInfo(f'event2: {eventOutput}')
 
 		event = eventList[1]
-		givenDate, givenTime = eventList[1]["start"]["time"].split("T")
-		hour, minute, junk = givenTime.split(":", 2)
-
-		time = hour
-		if int(hour) == 0:
-			time = "12"
-		elif (int(hour) > 12):
-			time = str(int(hour) - 12)
-
-		ampm = "a m"
-		if int(hour) >= 12:
-			ampm = "p m"
-
-		if int(minute) > 0:
-			time = time + ' ' + minute
-
-		time = time + ' ' + ampm
-
+		time = self.formatTimeToVoice(time=eventList[1]["start"]["time"])
 		self.logInfo(f'time2: {time}')
 		self.endDialog(session.sessionId, f'The next event is {event["summary"]}. It will begin at {time}')
